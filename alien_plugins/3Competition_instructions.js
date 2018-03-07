925e3a422ac3ddbc7f77b67f9fc99402ab5ce670
Maria <maria.eckstein@berkeley.edu>
2018-03-07 14:57:03 -0800

    update competition instructions

--- alien_plugins/3Competition_instructions.js
+++ alien_plugins/3Competition_instructions.js
@@ -21,10 +21,11 @@ exmpl_season_buttons =
 
 intro_slide_season =
   "<center><p>You will first select which <i>seasons</i> you want for the competition. " +
-  "For example, you might see the two seasons below. Which one would you pick for yourself?</p>" +
-  "<p><i>In the competititon, you will press 'j' for the season on top and 'l' for the season on the bottom.</i>.</p>" +
-  exmpl_season_buttons +
-  "<p>Your opponent will automatically get the other season!"
+  "For example, you might see the two seasons below. <i>Pick the better one for yourself!</i></p>" +
+  "<p>In the competititon, you will press 'j' for the season on top and 'l' for the season on the bottom.</p>" +
+  "<p>Click 'Next' to pick your seasons!</p></center>" +
+  exmpl_season_buttons
+
 
 exmpl_alien_season_buttons =
   "<center><div class='response_buttons'>" +
@@ -34,9 +35,9 @@ exmpl_alien_season_buttons =
 
 intro_slide_alien_same_season =
   "<center><p>You can now select which <i>aliens</i> you want in each season. " +
-  "For example, you might see the two aliens in the season below. Which alien would you pick in this season?</p>" +
-  "<p><i>In the competititon, you will press 'j' for the alien on top and 'l' for the alien on the bottom.</i>.</p>" +
-  "<p>Your opponent will automatically get the alien you did not select.</p>" +
+  "For example, you might see the two aliens in the season below. <i>Pick the better one for yourself!</i></p>" +
+  "<p>In the competititon, you will press 'j' for the alien on top and 'l' for the alien on the bottom.</p>" +
+  "<p>Click 'Next' to pick your aliens!</p></center>" +
   exmpl_alien_season_buttons
 
 exmpl_alien_buttons =
@@ -47,10 +48,10 @@ exmpl_alien_buttons =
 
 intro_slide_alien =
   "<center><p>You can now select which <i>aliens</i> you want in general, that is, independent of the season. " +
-  "For example, you might see the two aliens below. Which one would you pick?</p>" +
-  "<p><i>In the competititon, you will press 'j' for the alien on the left and 'l' for the alien on the right.</i>.</p>" +
-  exmpl_alien_buttons +
-  "<p>Your opponent will automatically get the other alien! "
+  "For example, you might see the two aliens below. <i>Pick the better one for yourself!</i></p>" +
+  "<p>In the competititon, you will press 'j' for the alien on the left and 'l' for the alien on the right.</p>" +
+  "<p>Click 'Next' to pick your aliens!</p></center>" +
+  exmpl_alien_buttons
 
 exmpl_item_buttons =
   "<center><div class='response_buttons'>" +
@@ -60,10 +61,10 @@ exmpl_item_buttons =
 
 intro_slide_item =
   "<center><p>You can now select which <i>items</i> you want for the competition. " +
-  "For example, you might see the two items below. Which one would you pick?</p>" +
-  "<p><i>In the competititon, you will press 'j' for the item on the left and 'l' for the item on the right.</i>.</p>" +
-  exmpl_item_buttons +
-  "<p>Your opponent will automatically get the other item! "
+  "For example, you might see the two items below. <i>Pick the better one for yourself!</i></p>" +
+  "<p>In the competititon, you will press 'j' for the item on the left and 'l' for the item on the right.</p>" +
+  "<p>Click 'Next' to pick your items!</p></center>" +
+  exmpl_item_buttons
 
 exmpl_alien_season_buttons_rest =
   "<center><div class='response_buttons'>" +
@@ -73,12 +74,12 @@ exmpl_alien_season_buttons_rest =
 
 intro_slide_alien_rest_season =
   "<center><p>Last in the selection process, " +
-  "you can indicate which <i>combinations of aliens and seasons</i> you would prefer for the competition. " +
+  "you can indicate which <i>combinations of aliens and seasons</i> you want for the competition. " +
   "For example, you might see two aliens in two different seasons, like below. " +
   "Which combination of alien and season would you pick?</p>" +
-  "<p><i>In the competititon, you will press 'j' for the alien on the left and 'l' for the alien on the right.</i>.</p>" +
-  exmpl_alien_season_buttons_rest +
-  "<p>Your opponent will automatically get the other one!"
+  "<p>In the competititon, you will press 'j' for the alien on the left and 'l' for the alien on the right.</p>" +
+  "<p>Click 'Next' to pick your aliens!</p></center>" +
+  exmpl_alien_season_buttons_rest
 
 // Put instruction slides together
 intro_alien_same_seasonn = {
